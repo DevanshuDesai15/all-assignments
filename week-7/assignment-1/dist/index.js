dc6e23c3d220db76ac3ca2cee016085236c056c7
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-08-22 16:09:55 -0700

    fix: fixed imports in the files

--- week-7/assignment-1/dist/index.js
+++ week-7/assignment-1/dist/index.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const auth_1 = __importDefault(require("./routes/auth"));
+const todo_1 = __importDefault(require("./routes/todo"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
 const port = 3000;
-const authRoutes = require("./routes/auth");
-const todoRoutes = require("./routes/todo");
-const cors = require("cors");
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authRoutes);
-app.use("/todo", todoRoutes);
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/auth", auth_1.default);
+app.use("/todo", todo_1.default);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-mongoose.connect('mongodb://127.0.0.1/todos', { dbName: "todos" });
+mongoose_1.default.connect('mongodb://127.0.0.1/todos', { dbName: "todos" });
